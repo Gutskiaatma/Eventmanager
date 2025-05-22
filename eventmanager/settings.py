ab9e9ece912f1cf4b25a6f9321b82a58ef689f94
Gutskiaatma <mjbhavit1234@gmail.com>
2025-05-22 18:21:00 +0530

    Update settings.py

--- eventmanager/settings.py
+++ eventmanager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-85v07@-)l5zk(i^=ghooary8v)+=yj4gdij1@-vujne(19wzup
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["events.onrender.com"]
 
 
 # Application definition
